578b1c9cd140d597716f794722a7a5b0193effa0
Мария Ковтун <m.kovtun@gge.ru>
2024-02-16 23:53:15 +0300

    done

---
+++ src/App.tsx
@@ -0,0 +1,78 @@
+import { useState } from 'react'
+import './App.css'
+import StepsForm from './components/StepsForm'
+import {faTrashAlt} from "@fortawesome/free-regular-svg-icons";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+function compare(a: {steps:number,date:Date}, b: {steps:number,date:Date}) {
+  var dateA = a.date;
+  var dateB = b.date;
+ 
+  if (dateB.getTime() == dateA.getTime()) return 0;
+  else {
+    if (dateB.getTime() > dateA.getTime()) return -1; 
+    else return 1;
+  }
+}
+
+function App() {
+  const [state,setState] = useState<{steps:number,date:Date}[]>([]);
+
+  const handleSubmit = (newRow: {steps: number,date: Date}) => {
+    let rowToEdit = state.find(item => item.date == newRow.date);
+    if (typeof rowToEdit === "undefined") {
+      setState((prevState) => prevState.concat([newRow]));
+    } else {
+      const newState = state.map((currentRow) => (
+        currentRow.date == newRow.date 
+          ? { ...currentRow, steps: (rowToEdit.steps + newRow.steps)}
+          : currentRow
+      ));
+      setState(newState);
+    }
+  }
+
+  const handleDelete = (index: number) => {
+    let currentState = state;
+    currentState.splice(index,1);
+    setState(currentState);
+  }
+
+  const TableRows = () => {
+    return (<tbody>
+      {state.sort(compare).map((item,index) => 
+      <tr key={index}>
+        <td>
+          {`${item.date.getDate()}-${item.date.getMonth()+1}-${item.date.getFullYear()}`}
+        </td>
+        <td>
+          {item.steps}
+        </td>
+        <td>
+        <FontAwesomeIcon icon={faTrashAlt} onClick={() => handleDelete(index)}/>
+        </td>
+      </tr>
+      )}
+    </tbody>)
+    }
+
+  return (
+    <>
+    <StepsForm onSubmit={handleSubmit}/>
+    <table>
+      <thead>
+      <tr>
+        <th>Дата</th>
+        <th>Пройдено км</th>
+        <th>Действия</th>
+      </tr>
+      </thead>
+      <TableRows/>
+    </table>
+    </>
+  )
+}
+
+
+
+export default App
---
+++ src/components/StepsForm.tsx
@@ -0,0 +1,39 @@
+import { useState } from "react";
+
+type FormProps = {
+    onSubmit: (newRow:{steps:number,date:Date}) => void
+}
+ 
+ 
+const StepsForm = (props: FormProps) => {
+    const [state,setState] = useState<{steps:number,date:Date}>({steps:0,date:new Date()});
+    
+    const handleDateChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {name,value} = event.target;
+        setState(prevState => ({...prevState, date: new Date(value)}));
+     } 
+
+     const handleStepsChanged = (event: React.ChangeEvent<HTMLInputElement>) => {
+        const {name,value} = event.target;
+        setState(prevState => ({...prevState,steps:+value}))
+     }
+
+    return (
+    <form onSubmit={(e: React.SyntheticEvent) => {
+        e.preventDefault();
+        props.onSubmit(state);
+    }}>
+        <label htmlFor="date">
+            Дата
+            <input type='date' id='date' name='date' onChange={handleDateChanged}/>
+        </label>
+        <label htmlFor="km">
+            Пройдено, км
+            <input type='text' id='km' name='km' value={state?.steps} onChange={handleStepsChanged}/>
+        </label>
+        <input type="submit" value="OK"/>
+</form>
+)
+}
+
+export default StepsForm
